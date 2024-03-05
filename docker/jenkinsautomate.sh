8da1cadfe1a5a05d82f8dba5d2f955c990002266
ravirajvaghela <rvaghela@codal.com>
2024-03-05 17:44:39 +0530

    update

---
+++ docker/jenkinsautomate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dateofdeploy=$1
+isnewberunning="false"
+echo "is it running ? $isnewberunning"
+retrybecount=0
+while [ "$isnewberunning" != "true true true" ]; do
+   if [ "$retrybecount" = "200" ]; then
+       exit 1;
+   fi
+   sleep 10;
+   ((retrybecount++))
+   echo "BE running checking : $retrybecount times"
+   isnewberunning=$(kubectl get pods --selector=date="$dateofdeploy" -n accis -o jsonpath='{.items[*].status.containerStatuses[0].ready}')
+   echo "is it running ? $isnewberunning"
+done
