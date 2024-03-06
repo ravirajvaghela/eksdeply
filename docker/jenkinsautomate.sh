3a81aae2a462a2f8ebf3cb6702b09827f0d93d04
ravirajvaghela <rvaghela@codal.com>
2024-03-06 13:53:50 +0530

    udate

--- docker/jenkinsautomate.sh
+++ docker/jenkinsautomate.sh
@@ -4,13 +4,13 @@ dateofdeploy=$1
 isnewberunning="false"
 echo "is it running ? $isnewberunning"
 retrybecount=0
-while [ "$isnewberunning" != "true true true" ]; do
-   if [ "$retrybecount" = "200" ]; then
-       exit 1;
-   fi
-   sleep 10;
-   ((retrybecount++))
-   echo "BE running checking : $retrybecount times"
-   isnewberunning=$(kubectl get pods --selector=date="$dateofdeploy" -n accis-app -o jsonpath='{.items[*].status.containerStatuses[0].ready}')
-   echo "is it running ? $isnewberunning"
+while [[ "$isnewberunning" != *"true"* ]]; do
+  if [ "$retrybecount" = "400" ]; then
+    exit 1;
+  fi
+  sleep 10;
+  ((retrybecount++))
+  echo "$dateofdeploy running checking : $retrybecount times"
+  isnewberunning=$(kubectl get pods --selector=date="$dateofdeploy" -n mccpd -o jsonpath='{.items[*].status.containerStatuses[0].ready}')
+  echo "is it running ? $isnewberunning"
 done
